43a47f300cfee533fc4b7b536fd82ad5adb46184
Dmytro Savitskyi <zefir0211@gmail.com>
2023-05-25 20:23:01 +0200

    Пробний

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const itemContainer = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("");
+itemContainer.insertAdjacentHTML("beforeend", markup);
