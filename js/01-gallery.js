28d6aa0288c7fd6104fa4ccc36555aaf3612d6cf
Dmytro Savitskyi <zefir0211@gmail.com>
2023-06-02 18:36:26 +0200

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
 const itemContainer = document.querySelector('.gallery');
 const markup = galleryItems.map(({preview, original, description}) => 
